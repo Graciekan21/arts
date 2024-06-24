95b6c84fae4cf030ccd05f07ae1cdd0c708d5f1b
Gracie Nemusesu <graciekan21@gmail.com>
2024-06-24 13:29:06 +0000

    Changed the Urls for the CLIENT_ORIGN_DEV and cganged the REGEXES

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://arts-afro-4626ca68624f.herokuapp.com/'
+axios.defaults.baseURL ='https://learnapi-91da03df4b64.herokuapp.com/' 
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true 
