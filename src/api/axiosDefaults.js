4a27e89e13579957c9133ca373d7c5998c07df3f
Gracie Nemusesu <graciekan21@gmail.com>
2024-06-24 21:52:24 +0000

    created a SignInnForm.js and created a SignInForm and error field

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL ='https://learnapi-91da03df4b64.herokuapp.com/' 
+axios.defaults.baseURL ='https://learnapi-91da03df4b64.herokuapp.com'
+// axios.defaults.baseURL = 'https://8000-graciekan21-learnapi-uh3mpkq66ly.ws.codeinstitute-ide.net' 
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true 
