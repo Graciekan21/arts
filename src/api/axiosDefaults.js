b5e080bec7fb18ff26517658d8613e4eba40dcfc
Gracie Nemusesu <graciekan21@gmail.com>
2024-06-25 11:09:32 +0000

    Put the correct URL to connent the api to the react app

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 axios.defaults.baseURL ='https://learnapi-91da03df4b64.herokuapp.com'
-// axios.defaults.baseURL = 'https://8000-graciekan21-learnapi-uh3mpkq66ly.ws.codeinstitute-ide.net' 
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true 
