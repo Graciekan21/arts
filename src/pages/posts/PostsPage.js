315247c465a943fc795d628e3d79e2399cc29664
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-07 16:37:33 +0000

    Createted a function that is reusable with infinite-Scroll to keep loading data from api

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,5 +1,3 @@
-import reactRouterDom from "react-router-dom";
-
 import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
@@ -16,6 +14,8 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -23,7 +23,6 @@ function PostsPage({ message, filter = "" }) {
   const { pathname } = useLocation();
 
   const [query, setQuery] = useState("");
-  
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -44,14 +43,14 @@ function PostsPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter,query, pathname]);
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-      <p>Popular profiles mobile</p>
-      <i className={`fas fa-search ${styles.SearchIcon}`} />
-      <Form
+        <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
           className={styles.SearchBar}
           onSubmit={(event) => event.preventDefault()}
         >
@@ -63,12 +62,19 @@ function PostsPage({ message, filter = "" }) {
             placeholder="Search posts"
           />
         </Form>
+
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map((post) => (
-                <Post key={post.id} {...post} setPosts={setPosts} />
-              ))
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
