3035b5e7a80ab51c4e32b49f65fca33d3449d392
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-08 05:20:57 +0000

    added a MoreDropdown to display edit and delete option to owners

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -5,7 +5,30 @@ import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
 
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, updated_at, content } = props;
+  const { profile_id, profile_image, owner, updated_at, content, id,
+    setPost,
+    setComments,} = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -19,6 +42,9 @@ const Comment = (props) => {
           <span className={styles.Date}>{updated_at}</span>
           <p>{content}</p>
         </Media.Body>
+        {is_owner && (
+          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        )}
       </Media>
     </div>
   );
